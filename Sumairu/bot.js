f32cc689e45801e039cb36f9260076e208e323d2
nhathaobui <nhathao_bui@msn.com>
2021-06-27 01:36:06 +0200

    Added sup bitch command

--- Sumairu/bot.js
+++ Sumairu/bot.js
@@ -1,51 +1,46 @@
-const Discord = require('discord.js');
-const logger = require('winston');
-const auth = require('./auth.json');
+const Discord = require("discord.js");
+const logger = require("winston");
+const auth = require("./auth.json");
 const fetch = require("node-fetch");
 
 // Configure logger settings
 logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-    colorize: true
+logger.add(new logger.transports.Console(), {
+  colorize: true,
 });
-logger.level = 'debug';
+logger.level = "debug";
 
 // Initialize Discord Bot
 var bot = new Discord.Client();
-bot.on('ready', () => {
-    logger.info('Logged in as ' + bot.user.username);
-
-    let ch = bot.channels.array();
-    let chUsers = bot.users.array();
-    console.log("Amount of channels LootBox: " + ch.length)
-    //console.log(ch[2].id)
-    /*chUsers.forEach((user,index) => {
-        console.log(user.username + ' ' + index);
-    })*/
-
-
+bot.on("ready", () => {
+  logger.info("Logged in as " + bot.user.username);
 });
 
-bot.on('message', (message) => {
-    var content = message.content;
-    var messageId = message.id;
-    var channel= message.channel;
-    var author = message.author;
-    var apiToken = "RGAPI-42f4e3ff-14eb-4e2f-b2e7-993842f250fb"; //Renew after each day
+bot.on("message", (message) => {
+  var content = message.content;
+  var messageId = message.id;
+  var channel = message.channel;
+  var author = message.author;
+  var apiToken = "RGAPI-42f4e3ff-14eb-4e2f-b2e7-993842f250fb"; //Renew after each day
 
-    var helpMsg = [
-        "These are the following commands to this bot in alpha version ",
+  var helpMsg = [
+    "These are the following commands to this bot in alpha version ",
+  ];
 
-    ];
+  if (content.startsWith("!")) {
+    var cmd = content.split(" ");
 
-    if(content.startsWith('!'))
-        var cmd = content.split(" ");
-        console.log(cmd[0].substring(1))
-        switch(cmd[0].substring(1)){
-            case 'help' : message.author.send(helpMsg); break;
-        }
-
-})
+    console.log(cmd[0].substring(1));
+    switch (cmd[0].substring(1)) {
+      case "help":
+        message.author.send(helpMsg);
+        break;
+      case "sup":
+        channel.send("sup bitch");
+        break;
+    }
+  }
+});
 
 /*bot.on('message',  (user, userID, channelID, message, evt) => {
     console.log(message.author);
